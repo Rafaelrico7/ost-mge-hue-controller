a1e8fd3faea551d76954349b9c4e85b3d4b2fa4c
Rafaelrico7 <rafael.fuhrer@ost.ch>
2020-10-24 20:11:32 +0200

    added settings activity

---
+++ app/src/main/java/com/example/hue/SettingsActivity.kt
@@ -0,0 +1,42 @@
+package com.example.hue
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+import kotlinx.android.synthetic.main.settings_activity.*
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    fun getBridgeIP(view: View){
+        val text = findViewById<EditText>(R.id.editText)
+        val ip = editText.text.toString()
+
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+}
